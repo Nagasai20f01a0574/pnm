fe162da1e89e3d03a9f3ae2c2679ea8e5c50b380
nagasai20f01a0574 <nagasai20f01a0574@gmail.com>
2023-06-20 01:56:13 +0530

    first commit

--- app.py
+++ app.py
@@ -1,59 +1,85 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import mysql.connector
-import datetime
-import os
+from datetime import date
 
 app = Flask(__name__)
-app.secret_key = os.urandom(24)
 
-# Database configuration
+# MySQL database connection configuration
 db = mysql.connector.connect(
     host="localhost",
     user="root",
     password="nagasai12p",
     database="pnm"
 )
-
 cursor = db.cursor()
 
-# Routes
+
+# Home page
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/employees', methods=['GET', 'POST'])
-def employees():
+
+# Add Employee
+@app.route('/add_employee', methods=['GET', 'POST'])
+def add_employee():
     if request.method == 'POST':
         name = request.form['name']
-        email = request.form['email']
-        cursor.execute("INSERT INTO employees (name, email) VALUES (%s, %s)", (name, email))
+        position = request.form['position']
+        salary = float(request.form['salary'])
+
+        # Insert employee data into the database
+        query = "INSERT INTO employees (name, position, salary) VALUES (%s, %s, %s)"
+        values = (name, position, salary)
+        cursor.execute(query, values)
         db.commit()
 
-    cursor.execute("SELECT * FROM employees")
-    employees = cursor.fetchall()
+        return redirect('/employees')
+    return render_template('add_employee.html')
 
+
+# Employees List
+@app.route('/employees')
+def employees():
+    query = "SELECT * FROM employees"
+    cursor.execute(query)
+    employees = cursor.fetchall()
     return render_template('employees.html', employees=employees)
 
+
+# Payroll Calculation
 @app.route('/payroll', methods=['GET', 'POST'])
-def payroll():
+def calculate_payroll():
     if request.method == 'POST':
         employee_id = request.form.get('employee_id')
         hours_worked = float(request.form['hours_worked'])
-        hourly_rate = float(request.form['hourly_rate'])
         deductions = float(request.form['deductions'])
-        pay_date = datetime.date.today().strftime('%Y-%m-%d')
 
-        gross_salary = hours_worked * hourly_rate
-        net_salary = gross_salary - deductions
+        # Fetch employee salary from the database
+        query = "SELECT salary FROM employees WHERE id = %s"
+        cursor.execute(query, (employee_id,))
+        result = cursor.fetchone()
 
-        cursor.execute("INSERT INTO payroll (employee_id, hours_worked, hourly_rate, deductions, gross_salary, net_salary, pay_date) VALUES (%s, %s, %s, %s, %s, %s, %s)",
-               (employee_id, hours_worked, hourly_rate, deductions, gross_salary, net_salary, pay_date))
-        db.commit()
+        if result:
+            salary = float(result[0])
+            gross_salary = hours_worked * salary
+            net_salary = gross_salary - deductions
+            pay_date = date.today()
+            # Insert payroll data into the database
+            query = "INSERT INTO payroll (employee_id, hours_worked, hourly_rate, deductions, gross_salary, net_salary, pay_date) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+            values = (employee_id, hours_worked, salary, deductions, gross_salary, net_salary, pay_date)
+            cursor.execute(query, values)
+            db.commit()
 
-    cursor.execute("SELECT employees.id, employees.name, employees.email, payroll.hours_worked, payroll.hourly_rate, payroll.deductions, payroll.gross_salary, payroll.net_salary, payroll.pay_date FROM employees INNER JOIN payroll ON employees.id = payroll.employee_id")
-    payroll = cursor.fetchall()
+            return redirect('/payroll')
+        else:
+            return "Employee not found"
+    
+    query = "SELECT p.*, e.name FROM payroll p JOIN employees e ON p.employee_id = e.id"
+    cursor.execute(query)
+    payroll_data = cursor.fetchall()
+    return render_template('payroll.html', payroll_data=payroll_data)
 
-    return render_template('payroll.html', payroll=payroll)
 
 if __name__ == '__main__':
     app.run(debug=True)
